d5491d21305a6fa1255f324d66d936697636a348
eroccked <eroccked@gmail.com>
2025-12-07 13:39:58 +0200

    Update SettingsViewModel.kt

--- app/src/main/java/com/powerschedule/app/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/powerschedule/app/ui/settings/SettingsViewModel.kt
@@ -49,7 +49,7 @@ class SettingsViewModel(application: Application) : AndroidViewModel(application
         _state.value = _state.value.copy(updateInterval = minutes)
         BackgroundUpdateWorker.schedule(getApplication(), minutes)
     }
-    
+
     suspend fun setNotificationMinutes(minutes: Int) {
         storageService.saveNotificationMinutes(minutes)
         _state.value = _state.value.copy(notificationMinutes = minutes)
